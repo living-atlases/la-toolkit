e192b6050d31083786938e2629364c026ddeef0e
vjrj <vjrj@ourproject.org>
2021-03-28 09:50:27 +0200

    Added miss preDeployCmd

---
+++ lib/models/preDeployCmd.dart
@@ -0,0 +1,42 @@
+import 'package:copy_with_extension/copy_with_extension.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import 'deployCmd.dart';
+
+part 'preDeployCmd.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+@CopyWith()
+class PreDeployCmd extends DeployCmd {
+  bool addUbuntuUser;
+  bool giveSudo;
+  bool etcHost;
+  bool solrLimits;
+
+  PreDeployCmd(
+      {this.addUbuntuUser = true,
+      this.giveSudo = true,
+      this.etcHost = true,
+      this.solrLimits = true,
+      limitToServers,
+      skipTags,
+      tags,
+      advanced = false,
+      continueEvenIfFails = false,
+      debug = false,
+      dryRun = false})
+      : super(
+            deployServices: ['all'],
+            limitToServers: limitToServers,
+            skipTags: skipTags,
+            tags: tags,
+            advanced: advanced,
+            onlyProperties: false,
+            continueEvenIfFails: continueEvenIfFails,
+            debug: debug,
+            dryRun: dryRun);
+
+  factory PreDeployCmd.fromJson(Map<String, dynamic> json) =>
+      _$PreDeployCmdFromJson(json);
+  Map<String, dynamic> toJson() => _$PreDeployCmdToJson(this);
+}
---
+++ lib/models/preDeployCmd.g.dart
@@ -0,0 +1,78 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'preDeployCmd.dart';
+
+// **************************************************************************
+// CopyWithGenerator
+// **************************************************************************
+
+extension PreDeployCmdCopyWith on PreDeployCmd {
+  PreDeployCmd copyWith({
+    bool? addUbuntuUser,
+    dynamic? advanced,
+    dynamic? continueEvenIfFails,
+    dynamic? debug,
+    dynamic? dryRun,
+    bool? etcHost,
+    bool? giveSudo,
+    dynamic? limitToServers,
+    dynamic? skipTags,
+    bool? solrLimits,
+    dynamic? tags,
+  }) {
+    return PreDeployCmd(
+      addUbuntuUser: addUbuntuUser ?? this.addUbuntuUser,
+      advanced: advanced ?? this.advanced,
+      continueEvenIfFails: continueEvenIfFails ?? this.continueEvenIfFails,
+      debug: debug ?? this.debug,
+      dryRun: dryRun ?? this.dryRun,
+      etcHost: etcHost ?? this.etcHost,
+      giveSudo: giveSudo ?? this.giveSudo,
+      limitToServers: limitToServers ?? this.limitToServers,
+      skipTags: skipTags ?? this.skipTags,
+      solrLimits: solrLimits ?? this.solrLimits,
+      tags: tags ?? this.tags,
+    );
+  }
+}
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PreDeployCmd _$PreDeployCmdFromJson(Map<String, dynamic> json) {
+  return PreDeployCmd(
+    addUbuntuUser: json['addUbuntuUser'] as bool,
+    giveSudo: json['giveSudo'] as bool,
+    etcHost: json['etcHost'] as bool,
+    solrLimits: json['solrLimits'] as bool,
+    limitToServers: json['limitToServers'],
+    skipTags: json['skipTags'],
+    tags: json['tags'],
+    advanced: json['advanced'],
+    continueEvenIfFails: json['continueEvenIfFails'],
+    debug: json['debug'],
+    dryRun: json['dryRun'],
+  )
+    ..deployServices = (json['deployServices'] as List<dynamic>)
+        .map((e) => e as String)
+        .toList()
+    ..onlyProperties = json['onlyProperties'] as bool;
+}
+
+Map<String, dynamic> _$PreDeployCmdToJson(PreDeployCmd instance) =>
+    <String, dynamic>{
+      'deployServices': instance.deployServices,
+      'limitToServers': instance.limitToServers,
+      'skipTags': instance.skipTags,
+      'tags': instance.tags,
+      'advanced': instance.advanced,
+      'onlyProperties': instance.onlyProperties,
+      'continueEvenIfFails': instance.continueEvenIfFails,
+      'debug': instance.debug,
+      'dryRun': instance.dryRun,
+      'addUbuntuUser': instance.addUbuntuUser,
+      'giveSudo': instance.giveSudo,
+      'etcHost': instance.etcHost,
+      'solrLimits': instance.solrLimits,
+    };
