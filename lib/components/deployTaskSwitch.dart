f7cf38c0363f8a11270f06ef3502cf42971bb213
vjrj <vjrj@ourproject.org>
2021-03-31 09:06:08 +0200

    Share code between pre and post

---
+++ lib/components/deployTaskSwitch.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../laTheme.dart';
+import 'helpIcon.dart';
+
+class DeployTaskSwitch extends StatelessWidget {
+  final String title;
+  final String? help;
+  final bool initialValue;
+  final Function(bool) onChanged;
+
+  const DeployTaskSwitch({
+    Key? key,
+    required this.title,
+    this.help,
+    required this.initialValue,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SwitchListTile(
+        // contentPadding: EdgeInsets.zero,
+        value: initialValue,
+        title: Text(title,
+            style: TextStyle(color: LAColorTheme.laThemeData.hintColor)),
+        secondary: help != null ? HelpIcon(wikipage: help!) : null,
+        onChanged: (bool newValue) {
+          onChanged(newValue);
+        });
+  }
+}
