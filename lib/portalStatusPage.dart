d2afb797b7abd8ffd943fde74ec0600dac235c63
vjrj <vjrj@ourproject.org>
2021-06-08 18:23:33 +0200

    Bottom with different color

--- lib/portalStatusPage.dart
+++ lib/portalStatusPage.dart
@@ -110,7 +110,7 @@ class _PortalStatusPageState extends State<PortalStatusPage> {
               // https://github.com/hacktons/convex_bottom_bar/
               // {0: '99+', 1: Icons.assistant_photo, 2: Colors.redAccent},
               bottomNavigationBar: ConvexAppBar(
-                backgroundColor: Colors.black26,
+                backgroundColor: LAColorTheme.laPalette.shade500,
                 style: TabStyle.react,
                 items: [
                   TabItem(icon: Mdi.server, title: "Servers"),
