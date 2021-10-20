237a18fdf5e944de33bad70bae51d10a557c33b9
vjrj <vjrj@ourproject.org>
2021-10-20 22:01:52 +0200

    Code commented cleanup

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -22,16 +22,6 @@ import '../laTheme.dart';
 import '../models/preDeployCmd.dart';
 import '../routes.dart';
 
-/*
-class ListUtils {
-  static bool notNull(Object? o) => o != null;
-
-// What is the best way to optionally include a widget in a list of children
-// https://github.com/flutter/flutter/issues/3783
-  static List<Widget> listWithoutNulls(List<Widget?> children) =>
-      children.where(notNull).toList();
-}
-*/
 class AppUtils {
   static bool isDev() {
     return !kReleaseMode;
