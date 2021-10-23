6d87aa000178112203e0702830e9fafaac9bc4a7
vjrj <vjrj@ourproject.org>
2021-10-23 10:10:09 +0200

    Remove resize notification as the bug causing this was solved

--- lib/deployPage.dart
+++ lib/deployPage.dart
@@ -91,137 +91,111 @@ class _DeployPageState extends State<DeployPage> {
                               message: "Cancel the deploy"),
                           onPressed: () => vm.onCancel(vm.project)),
                     ]),
-                body: NotificationListener(
-                    onNotification:
-                        (SizeChangedLayoutNotification notification) {
-                      debouncer.run(() {
-                        setState(() {
-                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                              duration: Duration(seconds: 5),
-                              content: Text(
-                                  "Resizing this window will break the ansible terminal. Please do not resize this window during deploying until we found a fix.")));
-                        });
-                      });
-                      return true;
-                    },
-                    child: SizeChangedLayoutNotifier(
-                        child: ScrollPanel(
-                            withPadding: true,
-                            child: Row(
-                              children: <Widget>[
-                                Expanded(
-                                  flex: 1, // 10%
-                                  child: Container(),
-                                ),
-                                Expanded(
-                                    flex: 8, // 80%,
-                                    child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          const SizedBox(height: 20),
-                                          const ListTile(
-                                              title: Text(
-                                                  "Select which services you want to deploy:",
-                                                  style:
-                                                      TextStyle(fontSize: 16))),
-                                          ServicesChipPanel(
-                                              initialValue: cmd.deployServices,
-                                              services: LAService
-                                                  .removeSimpleServices(vm
-                                                      .project
-                                                      .getServicesAssignedToServers()),
-                                              isHub: vm.project.isHub,
-                                              onChange: (s) => setState(() =>
-                                                  cmd.deployServices = s)),
-                                          ListTile(
-                                              title: const Text(
-                                                'Advanced options',
-                                              ),
-                                              trailing: Switch(
-                                                  value: advanced,
-                                                  onChanged: (value) =>
-                                                      setState(() => cmd
-                                                          .advanced = value))),
-                                          if (advanced)
-                                            ServerSelector(
-                                                selectorKey:
-                                                    GlobalKey<FormFieldState>(),
-                                                title: "Deploy to servers:",
-                                                modalTitle:
-                                                    "Choose some servers if you want to limit the deploy to them",
-                                                placeHolder: "All servers",
-                                                initialValue:
-                                                    cmd.limitToServers,
-                                                hosts: vm.project
-                                                    .serversWithServices()
-                                                    .map((e) => e.name)
-                                                    .toList(),
-                                                icon: MdiIcons.server,
-                                                onChange: (limitToServers) =>
-                                                    setState(() =>
-                                                        cmd.limitToServers =
-                                                            limitToServers)),
-                                          if (advanced)
-                                            TagsSelector(
-                                                initialValue: cmd.tags,
-                                                selectorKey:
-                                                    GlobalKey<FormFieldState>(),
-                                                tags: TagsConstants.getTagsFor(
-                                                    vm.project
-                                                        .alaInstallRelease),
-                                                icon: MdiIcons.tagPlusOutline,
-                                                title: "Tags:",
-                                                placeHolder: "All",
-                                                modalTitle:
-                                                    "Select the tags you want to limit to:",
-                                                onChange: (tags) => setState(
-                                                    () => cmd.tags = tags)),
-                                          if (advanced)
-                                            TagsSelector(
-                                                initialValue: cmd.skipTags,
-                                                selectorKey:
-                                                    GlobalKey<FormFieldState>(),
-                                                tags: TagsConstants.getTagsFor(
-                                                    vm.project
-                                                        .alaInstallRelease),
-                                                icon: MdiIcons.tagOffOutline,
-                                                title: "Skip tags:",
-                                                placeHolder: "None",
-                                                modalTitle:
-                                                    "Select the tags you want to skip:",
-                                                onChange: (skipTags) =>
-                                                    setState(() => cmd
-                                                        .skipTags = skipTags)),
-                                          if (advanced)
-                                            const TipsCard(
-                                                text:
-                                                    '''Ansible tasks are marked with tags, and then when you run it you can use `--tags` or `--skip-tags` to execute or skip a subset of these tasks.''',
-                                                margin: EdgeInsets.zero),
-                                          if (advanced)
-                                            ListTile(
-                                                title: const Text(
-                                                  'Only deploy properties (service configurations)',
-                                                ),
-                                                trailing: Switch(
-                                                    value: cmd.onlyProperties,
-                                                    onChanged: (value) =>
-                                                        setState(() =>
-                                                            cmd.onlyProperties =
-                                                                value))),
-                                          if (advanced) const DefDivider(),
-                                          if (advanced)
-                                            ListTile(
-                                                title: const Text(
-                                                  'Show extra debug info',
-                                                ),
-                                                trailing: Switch(
-                                                    value: cmd.debug,
-                                                    onChanged: (value) =>
-                                                        setState(() => cmd
-                                                            .debug = value))),
-                                          if (advanced)
-                                            /*  Not necessary now
+                body: ScrollPanel(
+                    withPadding: true,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1, // 10%
+                          child: Container(),
+                        ),
+                        Expanded(
+                            flex: 8, // 80%,
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(height: 20),
+                                  const ListTile(
+                                      title: Text(
+                                          "Select which services you want to deploy:",
+                                          style: TextStyle(fontSize: 16))),
+                                  ServicesChipPanel(
+                                      initialValue: cmd.deployServices,
+                                      services: LAService.removeSimpleServices(
+                                          vm.project
+                                              .getServicesAssignedToServers()),
+                                      isHub: vm.project.isHub,
+                                      onChange: (s) => setState(
+                                          () => cmd.deployServices = s)),
+                                  ListTile(
+                                      title: const Text(
+                                        'Advanced options',
+                                      ),
+                                      trailing: Switch(
+                                          value: advanced,
+                                          onChanged: (value) => setState(
+                                              () => cmd.advanced = value))),
+                                  if (advanced)
+                                    ServerSelector(
+                                        selectorKey:
+                                            GlobalKey<FormFieldState>(),
+                                        title: "Deploy to servers:",
+                                        modalTitle:
+                                            "Choose some servers if you want to limit the deploy to them",
+                                        placeHolder: "All servers",
+                                        initialValue: cmd.limitToServers,
+                                        hosts: vm.project
+                                            .serversWithServices()
+                                            .map((e) => e.name)
+                                            .toList(),
+                                        icon: MdiIcons.server,
+                                        onChange: (limitToServers) => setState(
+                                            () => cmd.limitToServers =
+                                                limitToServers)),
+                                  if (advanced)
+                                    TagsSelector(
+                                        initialValue: cmd.tags,
+                                        selectorKey:
+                                            GlobalKey<FormFieldState>(),
+                                        tags: TagsConstants.getTagsFor(
+                                            vm.project.alaInstallRelease),
+                                        icon: MdiIcons.tagPlusOutline,
+                                        title: "Tags:",
+                                        placeHolder: "All",
+                                        modalTitle:
+                                            "Select the tags you want to limit to:",
+                                        onChange: (tags) =>
+                                            setState(() => cmd.tags = tags)),
+                                  if (advanced)
+                                    TagsSelector(
+                                        initialValue: cmd.skipTags,
+                                        selectorKey:
+                                            GlobalKey<FormFieldState>(),
+                                        tags: TagsConstants.getTagsFor(
+                                            vm.project.alaInstallRelease),
+                                        icon: MdiIcons.tagOffOutline,
+                                        title: "Skip tags:",
+                                        placeHolder: "None",
+                                        modalTitle:
+                                            "Select the tags you want to skip:",
+                                        onChange: (skipTags) => setState(
+                                            () => cmd.skipTags = skipTags)),
+                                  if (advanced)
+                                    const TipsCard(
+                                        text:
+                                            '''Ansible tasks are marked with tags, and then when you run it you can use `--tags` or `--skip-tags` to execute or skip a subset of these tasks.''',
+                                        margin: EdgeInsets.zero),
+                                  if (advanced)
+                                    ListTile(
+                                        title: const Text(
+                                          'Only deploy properties (service configurations)',
+                                        ),
+                                        trailing: Switch(
+                                            value: cmd.onlyProperties,
+                                            onChanged: (value) => setState(() =>
+                                                cmd.onlyProperties = value))),
+                                  if (advanced) const DefDivider(),
+                                  if (advanced)
+                                    ListTile(
+                                        title: const Text(
+                                          'Show extra debug info',
+                                        ),
+                                        trailing: Switch(
+                                            value: cmd.debug,
+                                            onChanged: (value) => setState(
+                                                () => cmd.debug = value))),
+                                  if (advanced)
+                                    /*  Not necessary now
                               ListTile(
                                     title: const Text(
                                       'Continue even if some service deployment fails',
@@ -230,30 +204,27 @@ class _DeployPageState extends State<DeployPage> {
                                         value: cmd.continueEvenIfFails,
                                         onChanged: (value) => setState(() =>
                                             cmd.continueEvenIfFails = value))), */
-                                            if (advanced)
-                                              ListTile(
-                                                  title: const Text(
-                                                    'Dry run (only show the ansible command)',
-                                                  ),
-                                                  trailing: Switch(
-                                                      value: cmd.dryRun,
-                                                      onChanged: (value) =>
-                                                          setState(() =>
-                                                              cmd.dryRun =
-                                                                  value))),
-                                          if (advanced)
-                                            TipsCard(
-                                                text:
-                                                    "This project is generated in the '${vm.project.dirName}' directory."),
-                                          LaunchBtn(
-                                              onTap: onTap, execBtn: execBtn),
-                                        ])),
-                                Expanded(
-                                  flex: 1, // 10%
-                                  child: Container(),
-                                )
-                              ],
-                            ))))));
+                                    if (advanced)
+                                      ListTile(
+                                          title: const Text(
+                                            'Dry run (only show the ansible command)',
+                                          ),
+                                          trailing: Switch(
+                                              value: cmd.dryRun,
+                                              onChanged: (value) => setState(
+                                                  () => cmd.dryRun = value))),
+                                  if (advanced)
+                                    TipsCard(
+                                        text:
+                                            "This project is generated in the '${vm.project.dirName}' directory."),
+                                  LaunchBtn(onTap: onTap, execBtn: execBtn),
+                                ])),
+                        Expanded(
+                          flex: 1, // 10%
+                          child: Container(),
+                        )
+                      ],
+                    ))));
       },
     );
   }
