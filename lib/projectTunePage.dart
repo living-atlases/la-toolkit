55074d7683fb165fd7d262f0da9cb2a694ac5a8d
vjrj <vjrj@ourproject.org>
2021-11-04 23:05:33 +0100

    Don't show ansible extra if not advanced

--- lib/projectTunePage.dart
+++ lib/projectTunePage.dart
@@ -18,7 +18,6 @@ import 'package:la_toolkit/routes.dart';
 import 'package:la_toolkit/utils/StringUtils.dart';
 import 'package:la_toolkit/utils/regexp.dart';
 import 'package:la_toolkit/utils/utils.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:mdi/mdi.dart';
 
 import 'components/GenericSelector.dart';
@@ -158,10 +157,14 @@ class _LAProjectTunePageState extends State<LAProjectTunePage> {
                   color: Colors.black,
                   activeColor: Colors.black,
                   style: TabStyle.react,
-                  items: const [
-                    TabItem(icon: MdiIcons.formatListGroup, title: "Variables"),
-                    TabItem(icon: Mdi.autorenew, title: "Software versions"),
-                    TabItem(icon: Mdi.formTextbox, title: "Ansible Extras"),
+                  items: [
+                    const TabItem(
+                        icon: Mdi.formatListBulletedSquare, title: "Variables"),
+                    const TabItem(
+                        icon: Mdi.autorenew, title: "Software versions"),
+                    if (project.advancedTune)
+                      const TabItem(
+                          icon: Mdi.formTextbox, title: "Ansible Extras"),
                   ],
                   initialActiveIndex: 0, //optional, default as 0
                   onTap: (int i) => setState(() => _tab = i),
