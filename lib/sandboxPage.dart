0303806551096fe3ff59e23015865cb1ad303234
vjrj <vjrj@ourproject.org>
2022-01-07 08:53:46 +0100

    More dropdown samples in sandbox

--- lib/sandboxPage.dart
+++ lib/sandboxPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:la_toolkit/components/GenericSelector.dart';
 import 'package:la_toolkit/components/laAppBar.dart';
+import 'package:la_toolkit/components/softwareSelector.dart';
 import 'package:latlong2/latlong.dart';
 
 import 'components/scrollPanel.dart';
@@ -58,6 +59,35 @@ class _SandboxPageState extends State<SandboxPage> {
                     onChange: (String v) {
                       print(v);
                     }),
+                SoftwareSelector(
+                  versions: const ["a", "b"],
+                  onChange: (String? value) {
+                    print(value);
+                  },
+                  label: 'Test',
+                ),
+                SoftwareSelector(
+                  versions: const ["a", "b"],
+                  onChange: (String? value) {
+                    print(value);
+                  },
+                  label: 'Test',
+                ),
+                DropdownButton<String>(
+                  value: 'Dept1',
+                  elevation: 16,
+                  icon: const Icon(Icons.arrow_drop_down_circle),
+                  isExpanded: true,
+                  items: <String>['Dept1', 'Dept2'].map((e) {
+                    return DropdownMenuItem(
+                      value: e,
+                      child: Text(e),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    print(value);
+                  },
+                ),
               ],
             ),
           )));
