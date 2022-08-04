30878d3d35e3b74387d788330e8b86cc17ddfded
vjrj <vjrj@ourproject.org>
2022-08-04 10:10:07 +0200

    Add additional migration notes

--- lib/models/dependencies.dart
+++ lib/models/dependencies.dart
@@ -200,10 +200,31 @@ class Dependencies {
     cas: {
       vc(">= 6.5.6-3"): const MigrationNotesDesc(
           text:
-              "You have to perform some additional steps if you are upgrading your auth services from previous versions",
+              "Check these additional migration steps if you are upgrading your auth services",
           url:
               "https://github.com/AtlasOfLivingAustralia/ala-cas-5/blob/develop/UPGRADE.MD")
     },
+    images: {
+      vc(">= 1.1"): const MigrationNotesDesc(
+          text:
+              "Check these additional migration steps if you are upgrading your images service",
+          url:
+              "https://github.com/AtlasOfLivingAustralia/documentation/wiki/Image-Service-migration")
+    },
+    logger: {
+      vc(">= 4.0"): const MigrationNotesDesc(
+          text:
+              "Check these additional migration steps if you are upgrading your logger service",
+          url:
+              "https://github.com/AtlasOfLivingAustralia/documentation/wiki/Logger-Service-migration")
+    },
+    pipelines: {
+      vc(">= 0.1"): const MigrationNotesDesc(
+          text:
+              "You'll need to perform some additional steps in order to use pipelines",
+          url:
+              "https://github.com/AtlasOfLivingAustralia/documentation/wiki/pipelines-extra-steps")
+    }
   };
 
   static final Map<String, String> defVersions2_0_11 = {
