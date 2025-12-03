d699e401cabb748f94e0407ae1d07171f1167e3f
vjrj <vjrj@comunes.org>
2025-12-03 11:06:44 +0100

    Fix type error

--- lib/redux/logging_middleware.dart
+++ lib/redux/logging_middleware.dart
@@ -4,7 +4,7 @@ import 'package:redux_logging/redux_logging.dart';
 
 enum LogLevel { none, actions, all }
 
-LoggingMiddleware<dynamic> customLogPrinter({
+LoggingMiddleware<dynamic> customLogPrinter<State>({
   Logger? logger,
   Level level = Level.INFO,
   MessageFormatter<State> formatter = LoggingMiddleware.singleLineFormatter,
